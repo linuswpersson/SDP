651f649b6ce797150869678ed4bb9e4eab81845b
Filip <Filip.Bergman.6714@student.uu.se>
2020-02-14 23:07:17 +0100

    försökte lägga till en scrollbar lista men det spårade lite grann

---
+++ public/host/js/EventOverviewVue.js
@@ -0,0 +1,27 @@
+Vue.directive('scroll', {
+  inserted: function (el, binding) {
+    let f = function (evt) {
+      if (binding.value(evt, el)) {
+        window.removeEventListener('scroll', f)
+      }
+    }
+    window.addEventListener('scroll', f)
+  }
+})
+
+// main app
+new Vue({
+  el: '#app',
+  methods: {
+    onScroll: function (evt, el) {
+      if (window.scrollY > 50) {
+        el.setAttribute(
+          'style',
+          'opacity: 1; transform: translate3d(0, -10px, 0)'
+        )
+      }
+      return window.scrollY > 100
+    }
+  }
+})
+

33f652a0090ea1ba1b891c4fed72de0835b1f82e
Filip <Filip.Bergman.6714@student.uu.se>
2020-02-20 14:54:17 +0100

    list

--- public/host/js/EventOverviewVue.js
+++ public/host/js/EventOverviewVue.js
@@ -1,27 +1,28 @@
-Vue.directive('scroll', {
-  inserted: function (el, binding) {
-    let f = function (evt) {
-      if (binding.value(evt, el)) {
-        window.removeEventListener('scroll', f)
-      }
-    }
-    window.addEventListener('scroll', f)
-  }
-})
+let users = [
+    {
+	"name": "Karl Gustav",
+	"gender": true
+    },
+    {
+	"name": "Mona Lisa",
+	"gender": false
+    },
+    {
+	"name": "August",
+	"gender": true
+    },
+    {
+	"name": "Malin",
+	"gender": false
+    }    
+]
+
+
 
 // main app
 new Vue({
-  el: '#app',
-  methods: {
-    onScroll: function (evt, el) {
-      if (window.scrollY > 50) {
-        el.setAttribute(
-          'style',
-          'opacity: 1; transform: translate3d(0, -10px, 0)'
-        )
-      }
-      return window.scrollY > 100
-    }
-  }
+    el: '#app',
+    data: { list: users
+	  }
 })
 

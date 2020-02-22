546a1dfe7ed699ccc31f9fc099ef6af18b61de96
Itsyaboiidon <jonnybravo_he@hotmail.com>
2020-02-23 00:01:10 +0100

    rating screen done

--- public/user/js/review_functions.js
+++ public/user/js/review_functions.js
@@ -1,5 +1,23 @@
- 
-function goBack() {
-  window.history.back();
-}
+const vm = new Vue({
+    el: '#buttons',
+    data: {
+	userHome: 'user_home.html',
+    },
+    methods: {
+	userHomeClick: function() {
+	    document.location.href = this.userHome;
+	},
+	
+    },
+})
 
+/* 5-star rating method*/
+document.querySelector('#rating').addEventListener('click', function (e) {
+    let action = 'add';
+    for (const span of this.children) {
+        span.classList[action]('active');
+        if (span === e.target) action = 'remove';
+    }
+});
+
+/* TODO: Implement saving of rating*/ 

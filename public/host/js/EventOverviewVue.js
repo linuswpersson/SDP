ea6dd22c54e3638b80cfc44a6511fba79afa2088
sighsigh80 <linus735@gmail.com>
2020-02-25 11:35:23 +0100

    Swap preliminärt funkar

--- public/host/js/EventOverviewVue.js
+++ public/host/js/EventOverviewVue.js
@@ -1,32 +1,80 @@
 new Vue({
     el: '#app',
     data: {
+	currMale : -1,
+	currFemale : -1,
+	
 	maleArray : [
-	    {name : 'Johan', match : 'u11', id : 'u1'},
-	    {name : 'Erik', match : 'u12', id : 'u2'},
-	    {name : 'Jalmar', match : 'u13', id : 'u3'},
-	    {name : 'Lars', match : 'u14', id : 'u4'},
-	    {name : 'Åke', match : 'u15', id : 'u5'},
-	    {name : 'Tor', match : 'u16', id : 'u6'},
-	    {name : 'Valdermar', match : 'u17', id : 'u7'},
-	    {name : 'Jan', match : 'u18', id : 'u8'},
-	    {name : 'Olle', match : 'u19', id : 'u9'},
-	    {name : 'Rolf', match : 'u20', id : 'u10'},
+	    {name : 'Johan', matchId : 10, id : 0},
+	    {name : 'Erik', matchId : 11, id : 1},
+	    {name : 'Jalmar', matchId : 12, id : 2},
+	    {name : 'Lars', matchId : 13, id : 3},
+	    {name : 'Åke', matchId : 14, id : 4},
+	    {name : 'Tor', matchId : 15, id : 5},
+	    {name : 'Valdermar', matchId : 16, id : 6},
+	    {name : 'Jan', matchId : 17, id : 7},
+	    {name : 'Olle', matchId : 18, id : 8},
+	    {name : 'Rolf', matchId : 19, id : 9},
 	],
 
 
-	    femaleArray : [
-            {name : 'Lina', match : 'u1', id : 'u11'},
-	    {name : 'Frida', match : 'u2', id : 'u12'},
-	    {name : 'Mona Lisa', match : 'u3', id : 'u13'},
-	    {name : 'Erika', match : 'u4', id : 'u14'},
-	    {name : 'Linn', match : 'u5', id : 'u15'},
-	    {name : 'Simone', match : 'u6', id : 'u16'},
-	    {name : 'Julia', match : 'u7', id : 'u17'},
-	    {name : 'Bennilina', match : 'u8', id : 'u18'},
-	    {name : 'Johanna', match : 'u9', id : 'u19'},
-	    {name : 'Stina', match : 'u10', id : 'u20'},
+	femaleArray : [
+            {name : 'Lina', matchId : 0, id : 10},
+	    {name : 'Frida', matchId : 1, id : 11},
+	    {name : 'Mona Lisa', matchId : 2, id : 12},
+	    {name : 'Erika', matchId : 3, id : 13},
+	    {name : 'Linn', matchId : 4, id : 14},
+	    {name : 'Simone', matchId : 5, id : 15},
+	    {name : 'Julia', matchId : 6, id : 16},
+	    {name : 'Bennilina', matchId : 7, id : 17},
+	    {name : 'Johanna', matchId : 8, id : 18},
+	    {name : 'Stina', matchId : 9, id : 19},
 	],
+    },
+    methods: {
+	maleClick: function(male) {
+	    /* check if only unselect or new */
+	    /* open profile-window */
+	    /* unselect prev */
+	    /* highlight */
+	    this.currMale = male.id;
+	},
+
+	femaleClick: function(female) {
+	    /* check if only unselect or new */
+	    /* open profile-window */
+	    /* unselect prev */
+	    /* highlight */
+	    this.currFemale = female.id;
+	},
+
+	rematch: function() {
+	    if(this.currMale >= 0 && this.currFemale >= 0){
+		for(var i = 0; i < this.femaleArray.length; i++) {
+		    if(this.currFemale == this.femaleArray[i].id) {
+			break;
+		    }
+		}
+		for(var ii = 0; ii < this.maleArray.length; ii++) {
+		    if(this.currMale == this.maleArray[ii].id) {
+			break;
+		    }
+		}
+
+		var oldFemaleIndex = this.maleArray[ii].matchId - 10;
+		var oldMaleIndex = this.femaleArray[i].matchId;
+
+		this.femaleArray[i].matchId = ii;
+		this.maleArray[ii].matchId = i+10;
+		
+		this.femaleArray[oldFemaleIndex].matchId = oldMaleIndex;
+		this.maleArray[oldMaleIndex].matchId = oldFemaleIndex;
+
+		var tmp = this.femaleArray[oldFemaleIndex];
+		this.femaleArray.splice(oldFemaleIndex, 1, this.femaleArray[i]);
+		this.femaleArray.splice(i, 1, tmp);
+	    }
+	},
     },
 })
 

31f83a135655433b610210397de54568b6990125
Itsyaboiidon <jonnybravo_he@hotmail.com>
2020-02-25 12:28:58 +0100

    some stuff added

--- public/host/js/EventOverview.js
+++ public/host/js/EventOverview.js
@@ -9,6 +9,24 @@ let button;
 let Fbutton;
 
 
+function startTime() {
+  var today = new Date();
+  var h = today.getHours();
+  var m = today.getMinutes();
+  var s = today.getSeconds();
+  m = checkTime(m);
+  s = checkTime(s);
+  document.getElementById('clock').innerHTML =
+  h + ":" + m + ":" + s;
+  var t = setTimeout(startTime, 500);
+}
+function checkTime(i) {
+  if (i < 10) {i = "0" + i};  // add zero in front of numbers < 10
+  return i;
+}
+
+
+
 function openNav1() {
   document.getElementById("mySidenav").style.width = "500px";
   document.body.style.backgroundColor = "rgba(0,0,0,0.4)";

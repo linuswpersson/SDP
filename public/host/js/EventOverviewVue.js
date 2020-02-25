148c51c8a179a3da436527ac07624df5ea414a7b
sighsigh80 <linus735@gmail.com>
2020-02-25 08:50:47 +0100

    utseende good to go

--- public/host/js/EventOverviewVue.js
+++ public/host/js/EventOverviewVue.js
@@ -1,68 +1,32 @@
-let users = [
-    {
-	"male": "Karl Gustav",
-	"female": "Mona Lisa",
-	"other": false,
-	"no": 0,
-	"noF": 8
-    },
-    {
-	"male": "August",
-	"female": "Malin",
-	"other": true,
-	"no": 1,
-	"noF": 9
-    },
-    {
-	"male": "Karl Gustav",
-	"female": "Mona Lisa",
-	"other": false,
-	"no": 2,
-	"noF": 10
-    },
-    {
-	"male": "Karl",
-	"female": "Malin",
-	"other": true,
-	"no": 3,
-	"noF": 11
-    },
-    {
-	"male": "Karl Gustav",
-	"female": "Mona Lisa",
-	"other": false,
-	"no": 4,
-	"noF": 12
-    },
-    {
-	"male": "August",
-	"female": "Malin",
-	"other": true,
-	"no": 5,
-	"noF": 13
-    },
-    {
-	"male": "Karl Gustav",
-	"female": "Mona Lisa",
-	"other": false,
-	"no": 6,
-	"noF": 14
-    },
-    {
-	"male": "Lisa-Britt",
-	"female": "Malin",
-	"other": true,
-	"no": 7,
-	"noF": 15
-    }
-]
-
-
-
-// main app
 new Vue({
     el: '#app',
-    data: { list: users
-	  }
+    data: {
+	maleArray : [
+	    {name : 'Johan', match : 'u11', id : 'u1'},
+	    {name : 'Erik', match : 'u12', id : 'u2'},
+	    {name : 'Jalmar', match : 'u13', id : 'u3'},
+	    {name : 'Lars', match : 'u14', id : 'u4'},
+	    {name : 'Åke', match : 'u15', id : 'u5'},
+	    {name : 'Tor', match : 'u16', id : 'u6'},
+	    {name : 'Valdermar', match : 'u17', id : 'u7'},
+	    {name : 'Jan', match : 'u18', id : 'u8'},
+	    {name : 'Olle', match : 'u19', id : 'u9'},
+	    {name : 'Rolf', match : 'u20', id : 'u10'},
+	],
+
+
+	    femaleArray : [
+            {name : 'Lina', match : 'u1', id : 'u11'},
+	    {name : 'Frida', match : 'u2', id : 'u12'},
+	    {name : 'Mona Lisa', match : 'u3', id : 'u13'},
+	    {name : 'Erika', match : 'u4', id : 'u14'},
+	    {name : 'Linn', match : 'u5', id : 'u15'},
+	    {name : 'Simone', match : 'u6', id : 'u16'},
+	    {name : 'Julia', match : 'u7', id : 'u17'},
+	    {name : 'Bennilina', match : 'u8', id : 'u18'},
+	    {name : 'Johanna', match : 'u9', id : 'u19'},
+	    {name : 'Stina', match : 'u10', id : 'u20'},
+	],
+    },
 })
 

457e8f44f5d1f34f93553f35f6ab9fbc2fdfcda4
Filip <Filip.Bergman.6714@student.uu.se>
2020-03-02 22:20:17 +0100

    Started using messaging for reviews

--- public/user/js/review_functions.js
+++ public/user/js/review_functions.js
@@ -1,23 +1,48 @@
+'use strict';
+const socket = io();
+
+/* Have to construct something specific for one user before I can properly save reviews */
 const vm = new Vue({
     el: '#buttons',
     data: {
 	meetingList: 'meeting_list.html',
+	ratingMessage: '',
+	rating: '',
     },
     methods: {
 	userHomeClick: function() {
 	    document.location.href = this.meetingList;
 	},
-	
-    },
-})
+	//Sends the data to app.js, where it is stored.
+	saveRating: function(rating, message) {
+	    socket.emit('saveRating', rating, message, function(data) { 
+	//	console.log(rating); // data will be 'tobi says woot'
+		//console.log(message);
+	});
+	},
+	//Print is simply to confirm that everything has been stored in app.js.
+	//One could easily import them with socket.on
+	print: function() {
+	    socket.emit('printALL', function(print){
+		console.log(print);
+	    })
+	}
+    }})
 
 /* 5-star rating method*/
+//Hur incrementar jag detta?! Inte fokus atm.
 document.querySelector('#rating').addEventListener('click', function (e) {
     let action = 'add';
+    let rating = 0;
     for (const span of this.children) {
         span.classList[action]('active');
         if (span === e.target) action = 'remove';
+
     }
+    vm.saveRating(rating, vm.ratingMessage);
+    vm.print();
+   
 });
 
-/* TODO: Implement saving of rating*/ 
+
+    
